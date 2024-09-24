f5119fb14fae1193e339b35c5d9b40512af30a4e
fillerHomePc <fillerdk1234@gmail.com>
2024-09-24 21:19:01 +0200

    L07 sirpinski triangle updated

--- L07 - Rekursion/src/sirpinskiTriangles/Gui.java
+++ L07 - Rekursion/src/sirpinskiTriangles/Gui.java
@@ -1,9 +1,14 @@
 package sirpinskiTriangles;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
@@ -31,14 +36,38 @@ public class Gui extends Application {
         pane.setHgap(10);
         pane.setVgap(10);
 
-        int x = 50;
-        int y = 150;
-        int h = 100;
+        drawTriangle(pane, 1);
 
-        // Polygon with 3 points (200,100), (300,100) and (250,150)
-        Polygon polygon = new Polygon(x, y, x + h/2, y - h, x + h, y);
-        polygon.setFill(Color.WHITE);
-        polygon.setStroke(Color.BLACK);
-        pane.getChildren().add(polygon);
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.BASELINE_CENTER);
+        hBox.setSpacing(10);
+        pane.add(hBox, 0, 1);
+
+        Button btnDec = new Button("<");
+        Button btnAsc = new Button(">");
+        Label lblOrder = new Label("Order: 0");
+
+        hBox.getChildren().addAll(btnDec, lblOrder, btnAsc);
+    }
+
+    public void drawTriangle(GridPane pane, int order) {
+        int x = 50;
+        int y = 300;
+        int h = 300;
+
+        if (order == 0) {
+            // Polygon with 3 points (50,150), (100,50) and (150,150)
+            Polygon polygon = new Polygon(x, y, x + h, y - h, x + 2*h, y);
+            polygon.setFill(Color.TRANSPARENT);
+            polygon.setStroke(Color.BLACK);
+            pane.getChildren().add(polygon);
+        } else {
+            Polygon polygon = new Polygon(x, y, x + h/2, y - h/2, x + h, y);
+            polygon.setFill(Color.TRANSPARENT);
+            polygon.setStroke(Color.BLACK);
+            pane.getChildren().add(polygon);
+
+            drawTriangle(pane, order - 1);
+        }
     }
 }
