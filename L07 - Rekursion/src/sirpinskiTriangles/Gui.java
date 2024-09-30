c89f0a6ecdba264a3fa527828050958f908d511d
fillerHomePc <fillerdk1234@gmail.com>
2024-09-30 20:17:27 +0200

    L07 sirpinski progress

--- L07 - Rekursion/src/sirpinskiTriangles/Gui.java
+++ L07 - Rekursion/src/sirpinskiTriangles/Gui.java
@@ -23,10 +23,9 @@ public class Gui extends Application {
         Stage window = mainStage;
         window.setTitle("Sirpinski Triangles");
         GridPane pane = new GridPane();
-        pane.setMinSize(400, 400);
 
         initContent(pane);
-        Scene sirpinski = new Scene(pane);
+        Scene sirpinski = new Scene(pane, 800, 600);
 
         window.setScene(sirpinski);
         window.show();
@@ -36,6 +35,8 @@ public class Gui extends Application {
         pane.setPadding(new Insets(20));
         pane.setHgap(10);
         pane.setVgap(10);
+
+        drawTriangle(pane, 400, 300, 300, 2);
 
         HBox hBox = new HBox();
         hBox.setAlignment(Pos.BASELINE_CENTER);
@@ -47,29 +48,29 @@ public class Gui extends Application {
         Label lblOrder = new Label("Order: 0");
 
         hBox.getChildren().addAll(btnDec, lblOrder, btnAsc);
-
-        pane.add(drawTriangle(0), 0, 0);
-        pane.add(drawTriangle(1), 0, 0);
     }
 
-    public Polygon drawTriangle(int order) {
-        int x = 50;
-        int y = 300;
-        int h = 300;
-        Polygon p = null;
-
+    private void drawTriangle(GridPane pane, double x, double y, double size, int order) {
         if (order == 0) {
-            // Polygon with 3 points (50,150), (100,50) and (150,150)
-            p = new Polygon(x, y, x + h, y - h, x + 2*h, y);
-            p.setFill(Color.TRANSPARENT);
-            p.setStroke(Color.BLACK);
+            Polygon triangle = createTriangle(x, y, size);
+            triangle.setFill(null);
+            triangle.setStroke(Color.BLACK);
+            pane.getChildren().add(triangle);
         } else {
-            p = new Polygon(x, y, x + h/2, y - h/2, x + h, y);
-            p.setFill(Color.TRANSPARENT);
-            p.setStroke(Color.BLACK);
-
-            drawTriangle(order - 1);
+            double newSize = size / 2;
+            drawTriangle(pane, x, y, newSize, order - 1);
+            drawTriangle(pane, x - newSize, y + newSize, newSize, order - 1);
+            drawTriangle(pane, x + newSize, y + newSize, newSize, order - 1);
         }
-        return p;
+    }
+
+    private Polygon createTriangle(double x, double y, double size) {
+        Polygon triangle = new Polygon();
+        triangle.getPoints().addAll(
+                x, y,                           // top
+                x - size, y + size,         // venstre hjørne
+                x + size, y + size          // højre hjørne
+        );
+        return triangle;
     }
 }
