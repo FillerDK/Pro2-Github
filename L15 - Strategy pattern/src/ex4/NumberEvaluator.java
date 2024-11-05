39ef1f11748c4e1ee4b42ccf398f4655767236c7
fillerHomePc <fillerdk1234@gmail.com>
2024-11-05 21:23:38 +0100

    L15 opg 4 ongoing

---
+++ L15 - Strategy pattern/src/ex4/NumberEvaluator.java
@@ -0,0 +1,10 @@
+package ex4;
+
+public class NumberEvaluator implements Evaluator {
+    /** Returns true, if the param s contains a valid integer or decimal number. */
+    @Override
+    public boolean isValid(String s) {
+
+        return true;
+    }
+}
